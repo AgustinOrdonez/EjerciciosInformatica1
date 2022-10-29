f57b8b9ceca93d45ba21f55ac14826a69650f71b
dioswilson <aordonez@frba.utn.edu.ar>
2022-10-29 19:20:26 -0300

    Clase 28-10

---
+++ Clase 28-10/CosasClase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+
+void sigTerm(int);
+
+
+int main() {
+    signal(31, sigTerm);
+//    while (1) {
+//        sleep(1);
+//    }
+    pause();
+    return 0;
+}
+
+
+void sigTerm(int sig) {
+    printf("COSO");
+//    exit(0);
+}
